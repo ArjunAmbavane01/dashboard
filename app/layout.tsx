839d17336d95ff545706d6f9e7cddd08f9c847f5
Arjun <arjun.ambavane@gmail.com>
2024-12-16 09:58:40 +0530

    Theme Toggle Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import {Toaster} from '@/components/ui/toaster';
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={true} storageKey="dashboard-theme">
         <Navbar />
         <div className="flex">
           <div className="hidden md:block h-[100vh] w-[300px]">
@@ -36,6 +39,8 @@ export default function RootLayout({
           </div>
           <div className="p5 w-full md:max-w-[1140px]">{children}</div>
         </div>
+        <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { ThemeToggler } from '@/components/ThemeToggler';
 
 
 const Navbar = () => {
@@ -17,7 +18,10 @@ const Navbar = () => {
         <Link href='/'>
             <Image src={logo} alt='ArjunAmbavane' width={40} />
         </Link>
-        <DropdownMenu>
+
+        <div className="flex items-center">
+            <ThemeToggler />
+            <DropdownMenu>
             <DropdownMenuTrigger className='focus:outline-none'>
                 <Avatar>
                     <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
@@ -39,6 +43,8 @@ const Navbar = () => {
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
+        </div>
+       
     </div>);
 }
 
